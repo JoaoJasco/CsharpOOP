f1d53953abdc79ec2a1eec5d480776d577ae37fb
Jasco <jhjasco@gmail.com>
2025-03-10 16:29:48 -0300

    Criada primeira classe: conta bancaria;

--- ContaBancaria/Program.cs
+++ ContaBancaria/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria minhaConta = new ContaBancaria();
+      
+
+        Console.WriteLine("Saldo atual: " + minhaConta.saldo());
+    }
+}
+
+
+class ContaBancaria {
+    double valor = 0;
+
+    public void depositar (double deposito) {
+        if (deposito > 0) {
+            valor = valor + deposito;
+        }
+    }
+
+    public void sacar (double saque) {
+        valor = valor - saque;
+    }
+
+    public double saldo(){
+        return valor;
+    }
+
+}
+
--- ContaBancaria/obj/Debug/net9.0/CsharpOOP.AssemblyInfo.cs
+++ ContaBancaria/obj/Debug/net9.0/CsharpOOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpOOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94117b4ef43075dcf00ddb49071abc7c2d0029fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee00c46487298ec234b5aab32b310c913086c88")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpOOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpOOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
